b67fed4e25b4b2627e50e263544a5dc44aa30701
Fredrik Thune <thunef@users.noreply.github.com>
2018-10-06 17:57:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@ git config --global alias.minus reset -q
 git config --global alias.ta reset -q
 git config --global alias.ge add
 git config --global alias.allt add .
+git config --global alias.lappa add -p
 git config --global alias.knuffa push
 git config --global alias.tryck push
 git config --global alias.forgrena branch
 git config --global alias.gren branch
+git config --global alias.bega commit
 git config --global alias.forbind commit
 git config --global alias.anstifta commit
 git config --global alias.ymmpa rebase
@@ -18,11 +20,16 @@ git config --global alias.sammanfoga merge
 git config --global alias.mosa squash 
 git config --global alias.stoppa-undan stash
 git config --global alias.forvara stash
+# Det som göms i snö kommer upp i tö
+git config --global alias.to stash pop 
+git config --global alias.sno stash
 git config --global alias.lapp tag
+git config --global alias.mark tag
 git config --global alias.plocka-russin cherry-pick
 git config --global alias.fixa amend
 git config --global alias.klandra blame
 git config --global alias.apportera fetch
+git config --global alias.hamta fetch
 git config --global alias.visa status
 git config --global alias.skillnad diff
 git config --global alias.utforska checkout
