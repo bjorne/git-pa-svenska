6b825d7292c33a799ae122893e7ef771e6059281
Fredrik Thune <thunef@users.noreply.github.com>
2018-10-06 16:40:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
-git config --global alias.ryck pull
+git config --global alias.drag pull
+git config --global alias.ryck pull --rebase
 git config --global alias.plus add
-git config --global alias.knuff push
+git config --global alias.minus reset -q
+git config --global alias.ta reset -q
+git config --global alias.ge add
+git config --global alias.allt add .
+git config --global alias.knuffa push
+git config --global alias.tryck push
+git config --global alias.forgrena branch
 git config --global alias.gren branch
-git config --global alias.remittera commit
+git config --global alias.forbind commit
+git config --global alias.anstifta commit
 git config --global alias.ymmpa rebase
-git config --global alias.merge sammanfoga
-git config --global alias.squash mosa
+git config --global alias.sammanfoga merge 
+git config --global alias.mosa squash 
 git config --global alias.stoppa-undan stash
+git config --global alias.forvara stash
 git config --global alias.lapp tag
 git config --global alias.plocka-russin cherry-pick
-git config --global alias.fixa-till amend
+git config --global alias.fixa amend
 git config --global alias.klandra blame
 git config --global alias.apportera fetch
-
-FILE=~/.bashrc
-REGEX="^alias git='LANG=.* git'"
-DEST="alias git='LANG=sv_SE git'"
-
-if grep -qe "$REGEX" "$FILE"
-then
-  LINE=$(grep -ne "$REGEX" "$FILE" | cut -d : -f 1)
-  sed -i.bak -e $LINE'd' $FILE
-fi
-echo $DEST >> $FILE
-source $FILE
+git config --global alias.visa status
+git config --global alias.skillnad diff
+git config --global alias.utforska checkout
+git config --global alias.aterstall checkout
+git config --global alias.forinta reset --hard
