8cced505126eef9c1a7c845a0ed47db2a2ef2739
Fredrik Thune <thunef@users.noreply.github.com>
2019-12-05 10:30:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,6 +35,8 @@ git config --global alias.hamta fetch
 git config --global alias.visa status
 git config --global alias.skillnad diff
 git config --global alias.tillagt 'diff --staged'
+git config --global alias.rott diff
+git config --global alias.gront 'diff --staged'
 git config --global alias.utforska checkout
 git config --global alias.aterstall checkout
 git config --global alias.forinta 'reset --hard'
